9b6f8e1da676dcd1726171f367a774601941cd06
Mongoll <teras8602@gmail.com>
2023-01-14 22:27:18 +0200

    Updates

--- src/redux/actions/UiAction.ts
+++
@@ -1,9 +0,0 @@
-import { UPDATE_SEARCH_KEYWORD, SetSearchKeywordAction } from "../../types";
-
-//Add a country to cart
-export function setSearchKeyword(keyword: string): SetSearchKeywordAction {
-  return {
-    type: UPDATE_SEARCH_KEYWORD,
-    payload: keyword,
-  };
-}
--- src/redux/actions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./ProductAction";
-export * from "./CartAction";
-export * from "./UiAction";
-export * from "./CategoryAction";
-export * from "./UserAction";
